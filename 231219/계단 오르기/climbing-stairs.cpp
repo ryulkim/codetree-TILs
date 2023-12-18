0141b8632ae0356ed89aa3efe3cfbea293c94186
ryulkim <58160874+ryulkim@users.noreply.github.com>
2023-12-19 00:33:07 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231219/계단 오르기/climbing-stairs.cpp
+++ 231219/계단 오르기/climbing-stairs.cpp
@@ -9,15 +9,17 @@ int main() {
 
     fill(dp, dp+1004, 100000);
     
+    dp[0]=1;
     dp[2]=1;
     dp[3]=1;
+    dp[4]=1;
 
     
 
     for(int i=2;i<=n-2;i++){
         if(dp[i]==100000) continue;
-        dp[i+2]=min(dp[i+2],dp[i]+1);
-        dp[i+3]=min(dp[i+3],dp[i]+1);
+        dp[i+2]=min(dp[i+2],dp[i]+1)%10007;
+        dp[i+3]=min(dp[i+3],dp[i]+1)%10007;
     }
 
     if(dp[n]==100000)cout<<0;

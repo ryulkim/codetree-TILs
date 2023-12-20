a4f3a69ee00cfaab48eed5a3d4d0832d6887662b
ryulkim <58160874+ryulkim@users.noreply.github.com>
2023-12-21 01:29:47 +0900

    Title: 차이가 가장 작은 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231221/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m,x;
+    set<int> s;
+    vector<int> v;
+    int ans=2e9;
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        cin>>x;
+        v.push_back(x);
+        s.insert(x);
+    }
+
+    int sz=v.size();
+
+    for(int i=0;i<sz;i++){
+        //a-b>=m -> b<=a-m
+        set<int>::iterator iter;
+        iter=s.upper_bound(v[i]-m);
+        if(iter!=s.begin()){
+            iter--;
+            ans=min(ans, v[i]-*iter);
+        }
+
+        //b-a>=m -> b>=m+a
+        iter=s.lower_bound(m+v[i]);
+        if(iter!=s.end()){
+            ans=min(ans, *iter-v[i]);
+        }
+    }
+
+    if(ans==1e11){
+        cout<<-1;
+    }
+    else cout<<ans;
+
+    return 0;
+}

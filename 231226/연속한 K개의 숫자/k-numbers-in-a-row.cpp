2d53715b36efbc0b632d2c3a638076d98656678b
ryulkim <58160874+ryulkim@users.noreply.github.com>
2023-12-26 00:01:29 +0900

    Title: 연속한 K개의 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/연속한 K개의 숫자/k-numbers-in-a-row.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,k,b,x;
+    int arr[100004]={0,};
+    cin>>n>>k>>b;
+
+    for(int i=0;i<b;i++){
+        cin>>x;
+        arr[x]=1;
+    }
+
+    for(int i=1;i<=n;i++){
+        arr[i]+=arr[i-1];
+        //cout<<arr[i]<<' ';
+    }
+
+    int m=100004;
+
+    for(int i=1;i<n-k+1;i++){
+        //cout<<i+k<<' '<<i<<' '<<arr[i+k]<<' '<<arr[i-1]<<'\n';
+        m=min(m, arr[i+k-1]-arr[i-1]);
+    }
+
+    cout<<m;
+
+    return 0;
+}

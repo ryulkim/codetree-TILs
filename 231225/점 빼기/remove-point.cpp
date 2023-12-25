4be3297450c6fca77d73d448b29c661f32e9e1c9
ryulkim <58160874+ryulkim@users.noreply.github.com>
2023-12-25 22:42:42 +0900

    Title: 점 빼기, Time: 330ms, Memory: 6MB, Status: Passed - CodeTree

---
+++ 231225/점 빼기/remove-point.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n,m,x,y;
+    set<pair<int,int>> s;
+    set<pair<int,int>>::iterator iter;
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        cin>>x>>y;
+        s.insert({x,y});
+    }
+
+    for(int i=0;i<m;i++){
+        cin>>x;
+        iter=s.lower_bound({x,0});
+        if(iter!=s.end()){
+            cout<<(*iter).first<<' '<<(*iter).second<<'\n';
+            s.erase(iter);
+        }
+        else{
+            cout<<-1<<' '<<-1<<'\n';
+        }
+    }
+
+    return 0;
+}

d51e6e0cbba85ae13f9d83e539b6528b6226e8a2
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-01-01 13:27:17 +0900

    Title: 앞에서부터 삭제하기 2, Time: 34ms, Memory: 1MB, Status: Passed - CodeTree

--- 240101/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 240101/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -6,24 +6,24 @@ using namespace std;
 int main() {
     int n,k,x;
     vector<int> v;
+    priority_queue<int> pq;
     double ans=0;
+    int sum=0;
     cin>>n;
 
     for(int i=0;i<n;i++){
         cin>>x;
         v.push_back(x);
     }
-    for(int k=1;k<=n-2;k++){
-        double value=0;
-        priority_queue<int> pq;
-        for(int i=k;i<n;i++){
-            pq.push(-v[i]);
-            value+=v[i];
-        }
 
-        value-=-pq.top();
-        value/=n-k-1;
-        ans=max(ans,value);
+    sum+=v[n-1];
+    pq.push(-v[n-1]);
+
+    for(int k=n-2;k>=1;k--){
+        
+        pq.push(-v[k]);
+        sum+=v[k];
+        ans=max(ans,(double)(sum+pq.top())/(n-k-1));
     }
     cout<<fixed;
     cout.precision(2);

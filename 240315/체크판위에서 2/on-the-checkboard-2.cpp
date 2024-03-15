1450d9868e900fac65e732d88741c6e5516bdc96
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-15 23:29:02 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240315/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int r,c;
+char arr[20][20];
+int ans=0;
+
+int proc(int i, int j, char ch){
+    int cnt=0;
+    for(int a=i+1;a<r-1;a++){
+        for(int b=j+1;b<c-1;b++){
+            if(arr[a][b]!=ch){
+                //cout<<a<<' '<<b<<'\n';
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    cin>>r>>c;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    char start=arr[0][0];
+
+    for(int i=0;i<r-2;i++){
+        for(int j=0;j<c-2;j++){
+            if(arr[i][j]!=start){
+                //cout<<i<<' '<<j<<'\n';
+                ans+=proc(i,j, arr[i][j]);
+            }
+        }
+    }
+
+    cout<<ans;
+
+    return 0;
+}

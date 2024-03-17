d5bf1e89541a1fd41414de9eb34d4ab050bfd725
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-17 14:52:19 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240317/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240317/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -10,33 +10,11 @@ int main() {
     cin>>arr[0]>>arr[1]>>arr[2];
     int num=0;
 
-    while(1){
-        sort(arr,arr+3);
-        //연속일 경우, 그대로
-        if(arr[0]+1==arr[1]&&arr[1]+1==arr[2]) break;
-        //3개중 2개만 연속일 경우, 연속된 것 중 끝쪽 선택
-        else if(arr[0]+1==arr[1]){
-            arr[0]=(arr[1]+arr[2]+1)/2;
-            num++;
-        }
-        else if(arr[1]+1==arr[2]){
-            arr[2]=(arr[1]+arr[0]+1)/2;
-            num++;
-        }
-        //다 떨어져있을 경우, 가운데랑 거리가 먼 곳 선택
-        else{
-            if(arr[1]-arr[0]>arr[2]-arr[1]){
-                arr[0]=(arr[1]+arr[2]+1)/2;
-                num++;
-            }
-            else{
-                arr[2]=(arr[1]+arr[0]+1)/2;
-                num++;
-            }
-        }
-    }
+    sort(arr, arr+3);
 
-    cout<<num;
+    if(arr[0]+1==arr[1]&&arr[1]+1==arr[2]) cout<<0;
+    else if(arr[0]+2==arr[1]||arr[1]+2==arr[2]) cout<<1;
+    else cout<<2;
 
     return 0;
 }

2945cdc8dbe0045da6f271ac48502fa01a7e2135
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-21 13:50:35 +0900

    Title: 정수 찾기, Time: 248ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240321/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int n,m,x;
+unordered_set<int> u;
+
+int main() {
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        u.insert(x);
+    }
+
+    cin>>m;
+
+    for(int i=0;i<m;i++){
+        cin>>x;
+        if(u.find(x)!=u.end()) cout<<1<<'\n';
+        else cout<<0<<'\n';
+    }
+    return 0;
+}

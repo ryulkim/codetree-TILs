76755776c470439b514b701238c9c2c3baf3f232
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-21 13:21:03 +0900

    Title: 낮은 지점들, Time: 53ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240321/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int n,x,y,ans=0;
+unordered_map<int, int> u;
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>x>>y;
+        if(u.find(x)==u.end()) u[x]=y;
+        else{
+            if(u[x]>y) u[x]=y;
+        }
+    }
+
+    for(auto pair: u){
+        ans+=pair.second;
+    }
+
+    cout<<ans;
+
+    return 0;
+}

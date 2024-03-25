35836c61990b1127f26ba65dd142f8ba81bcafe3
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-25 23:48:53 +0900

    Title: 0에 가장 가까운 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/0에 가장 가까운 합/sum-closest-to-zero.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int n,x,ans=1e9;
+vector<int> pl, mi;
+
+void proc(){
+    int msz=mi.size();
+    int psz=pl.size();
+    int j=0;
+    int sum=abs(mi[0]+pl[0]);
+
+    for(int i=0;i<psz;i++){
+        while(j<msz&&sum>abs(pl[i]+mi[j+1])){
+            j++;
+            ans=min(ans, abs(pl[i]+mi[j]));
+        }
+    }
+}
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>x;
+        if(x>=0) pl.push_back(x);
+        else mi.push_back(x);
+    }
+
+    sort(pl.begin(), pl.end());
+    sort(mi.begin(), mi.end(),greater<int>());
+
+    if(pl.size()==0) cout<<-(mi[0]+mi[1]);
+    else if(mi.size()==0) cout<<pl[0]+pl[1];
+    else{
+        proc();
+        cout<<ans;
+    }
+
+    return 0;
+}

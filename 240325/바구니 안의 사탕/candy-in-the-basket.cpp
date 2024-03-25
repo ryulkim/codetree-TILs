9f611f59db28cc869ebaa470efe4f7c92dcde61c
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-25 23:06:14 +0900

    Title: 바구니 안의 사탕, Time: 136ms, Memory: 8MB, Status: Passed - CodeTree

--- 240325/바구니 안의 사탕/candy-in-the-basket.cpp
+++ 240325/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 #include <algorithm>
-#define SZ 1000005
+#define SZ 10000005
 using namespace std;
 
-int n,k,candy,bucket,ans=0;
-int arr[SZ];
+int n,k,candy,bucket;
+long long ans=0;
+long long arr[SZ];
 
 void proc(){
-    int sum=0;
+    long long sum=0;
 
     for(int i=0;i<=2*k;i++){
-        if(i>SZ) {
+        if(i>=SZ) {
             ans=sum;
             return;
         }
         sum+=arr[i];
     }
 
+
     int j=2*k;
     
 
-    for(int i=0;i<SZ-2*k-5;i++){
+    for(int i=0;i<SZ-2*k;i++){
         ans=max(ans,sum);
-        //cout<<ans<<' ';
 
         sum-=arr[i];
         j++;
         sum+=arr[j];
-
-        //cout<<arr[i]<<' '<<arr[j]<<'\n';
     }
 }
 
 int main() {
     cin>>n>>k;
 
+    int sum=0;
     for(int i=0;i<n;i++){
         cin>>candy>>bucket;
-        arr[bucket-1]+=candy;
+        arr[bucket]+=candy;    
     }
 
     proc();

e6b3d97e59bd4a47d96921f5e6e09b2cd6d8f7de
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-25 22:21:08 +0900

    Title: 바구니 안의 사탕, Time: 86ms, Memory: 4MB, Status: Runtime Error - CodeTree

---
+++ 240325/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#define SZ 1000005
+using namespace std;
+
+int n,k,candy,bucket,ans=0;
+int arr[SZ];
+
+void proc(){
+    int sum=0;
+
+    for(int i=0;i<=2*k;i++){
+        if(i>SZ) {
+            ans=sum;
+            return;
+        }
+        sum+=arr[i];
+    }
+
+    int j=2*k;
+    
+
+    for(int i=0;i<SZ-2*k;i++){
+        ans=max(ans,sum);
+        //cout<<ans<<' ';
+
+        sum-=arr[i];
+        j++;
+        sum+=arr[j];
+
+        //cout<<arr[i]<<' '<<arr[j]<<'\n';
+    }
+}
+
+int main() {
+    cin>>n>>k;
+
+    for(int i=0;i<n;i++){
+        cin>>candy>>bucket;
+        arr[bucket-1]+=candy;
+    }
+
+    proc();
+
+    cout<<ans;
+
+    return 0;
+}

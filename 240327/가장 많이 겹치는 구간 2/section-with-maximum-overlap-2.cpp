d982f48357439fbf8bf70a0e9bdbeb2f1cf79f16
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-27 00:03:19 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 54ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240327/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,x1,x2,sum=0,ans=0;
+vector<pair<int, int>> v;
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>x1>>x2;
+        v.push_back({x1,1});
+        v.push_back({x2,-1});
+    }
+
+    sort(v.begin(),v.end());
+
+    for(int i=0;i<n;i++){
+        sum+=v[i].second;
+        ans=max(ans,sum);
+    }
+
+    cout<<ans;
+
+    return 0;
+}

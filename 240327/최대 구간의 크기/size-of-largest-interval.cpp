f41ee98773c9b155f93d52d85d60794ec0437d93
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-27 23:39:10 +0900

    Title: 최대 구간의 크기, Time: 146ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240327/최대 구간의 크기/size-of-largest-interval.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,a,b;
+vector<pair<int, int>> v;
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        v.push_back({a,1});
+        v.push_back({b,-1});
+    }
+
+    sort(v.begin(),v.end());
+
+    int sum=0,st=0;
+    long long ans=0;
+
+    for(int i=0;i<2*n;i++){
+        sum+=v[i].second;
+
+        if(sum==1&&v[i].second==1){
+            st=v[i].first;
+        }
+        else if(sum==0&&v[i].second==-1){
+            ans=max(ans,(long long) v[i].first-st);
+        }
+    }
+
+    cout<<ans;
+
+    return 0;
+}

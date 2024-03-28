7852b055d3c37a116d6134697a2243615e7220a5
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-03-29 01:31:41 +0900

    Title: 친구의 키, Time: 7ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240329/친구의 키/height-of-friends.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int n,m,a,b;
+vector<int> v[100005];
+stack<int> st;
+bool chk[100005];
+
+void dfs(int x){
+    for(int i=0;i<v[x].size();i++){
+        if(!chk[v[x][i]]){
+            chk[v[x][i]]=1;
+            dfs(v[x][i]);
+        }
+    }
+
+    st.push(x);
+}
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        v[a].push_back(b);
+    }
+
+    for(int i=1;i<=n;i++){
+        if(!chk[i]){
+            chk[i]=1;
+            dfs(i);
+        }
+    }
+
+    while(!st.empty()){
+        cout<<st.top()<<' ';
+        st.pop();
+    }
+
+    return 0;
+}

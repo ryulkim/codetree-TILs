76d939431aaccf7d68c44bda024c5fec1cf9e177
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-08 23:08:10 +0900

    Title: 트리 노드 제거, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240408/트리 노드 제거/remove-tree-node.cpp
+++ 240408/트리 노드 제거/remove-tree-node.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
-#include <unordered_set>
+#include <vector>
+#include <queue>
 #define SZ 55
 using namespace std;
 
 int n,x,ans=0;
-int par[SZ];
-int leaf[SZ];
-unordered_set<int> st;
+vector<int> v[SZ];
+bool visit[SZ];
+
+
+void bfs(int st){
+    queue<int> q;
+    q.push(st);
+    visit[st]=1;
+
+    while(!q.empty()){
+        int a=q.front();
+        int sz=v[a].size();
+        q.pop();
+        
+        if(sz==0) ans++;
+
+        for(int i=0;i<sz;i++){
+            int b=v[a][i];
+            if(!visit[b]) {
+                q.push(b);
+                visit[b]=1;
+            }
+        }
+    }
+}
 
 int main() {
     cin>>n;
 
     for(int i=0;i<n-1;i++){
-        cin>>par[i];
-        st.insert(i);
-        leaf[par[i]]++;
+        cin>>x;
+        if(x==-1) continue;
+        v[i].push_back(x);
+        v[x].push_back(i);
     }
 
     cin>>x;
-    st.erase(x);
+    v[x].clear();
 
-    for(int i=0;i<n;i++){
-        if(par[i]==x) par[i]==-1;
-        if(par[i]!=-1&&st.find(par[i])==st.end()){
-            ans++;
-        }
-    }
+    bfs(0);
 
     cout<<ans;
 

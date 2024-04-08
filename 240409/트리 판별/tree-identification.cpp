81fcd6e2949d7d9e7b139823a40d9795fe06a28f
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-09 00:27:44 +0900

    Title: 트리 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/트리 판별/tree-identification.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define SZ 10005
+using namespace std;
+
+int m,a,b, root, rnum=0, num=0, total=0;
+int indegree[SZ];
+bool visit[SZ];
+bool check[SZ];
+vector<int> v[SZ];
+queue<int> q;
+
+bool proc(){
+    for(int i=0;i<SZ;i++){
+        if(check[i]&&!indegree[i]){
+            root=i;
+            rnum++;
+        }
+    }
+
+    if(rnum>1) return 0;
+
+    q.push(root);
+    num=1;
+    visit[root]=1;
+
+    while(!q.empty()){
+        int a=q.front();
+        int sz=v[a].size();
+        q.pop();
+
+        for(int i=0;i<sz;i++){
+            int b=v[a][i];
+            if(!visit[b]){
+                visit[b]=1;
+                q.push(b);
+                num++;
+            }
+        }
+    }
+
+    //cout<<num;
+    if(num==total) return 1;
+    return 0;
+}
+
+int main() {
+    cin>>m;
+
+    for(int i=0;i<m;i++){
+        cin>>a>>b;
+        v[a].push_back(b);
+        indegree[b]++;
+        if(!check[a]) {
+            check[a]=1;
+            total++;
+        }
+        if(!check[b]) {
+            check[b]=1;
+            total++;
+        }
+    }
+
+
+    cout<<proc();
+    
+
+    return 0;
+}

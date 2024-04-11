2b903b86bc0401ec178b54d05af75c8262155137
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-12 04:17:55 +0900

    Title: 문자열 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/문자열 만들기/create-string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    int n;
+    string s,t="";
+
+    cin>>n>>s;
+
+    int sz=s.size();
+
+    int end=sz-1;
+    for(int i=0;i<end;i++){
+        while(1){
+            if(s[i]<s[end]) {
+                t+=s[i];
+                break;
+            }
+            t+=s[end--];
+        }
+    }
+
+    t+=s[end];
+
+    cout<<t;
+
+    return 0;
+}

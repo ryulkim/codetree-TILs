3429f5d7263230b5db26ec542645a788bcee2e11
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-12 04:42:58 +0900

    Title: 문자열 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/문자열 만들기/create-string.cpp
+++ 240412/문자열 만들기/create-string.cpp
@@ -11,11 +11,30 @@ int main() {
     int end=n-1;
     for(int i=0;i<end;i++){
         while(1){
-            if(s[i]<s[end]) {
+            if(s[i]==s[end]){
+                int a=i;
+                int b=end;
+                while(a<b&&s[a]==s[b]){
+                    a++; b--;
+                }
+                if(a>=b) {
+                    t+=s[i];
+                    break;
+                }
+                else if(s[a]<s[b]){
+                    t+=s[i];
+                    break;
+                }
+                else{
+                    t+=s[end--];
+                }
+            }
+            else if(s[i]<s[end]) {
                 t+=s[i];
                 break;
             }
             t+=s[end--];
+            if(i==end) break;
         }
     }
 

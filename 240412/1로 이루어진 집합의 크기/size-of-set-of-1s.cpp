17dd2f43dc9e0c8f23ff6bb4a12968a19891e226
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-12 01:07:08 +0900

    Title: 1로 이루어진 집합의 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240412/1로 이루어진 집합의 크기/size-of-set-of-1s.cpp
+++ 240412/1로 이루어진 집합의 크기/size-of-set-of-1s.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int n,m,ans=0;
 int arr[505][505];
+int visit[505][505];
 int dr[5]={0,0,1,-1};
 int dc[5]={1,-1,0,0};
 queue<pair<int,int>> q;
+int group=1;
+int group_size[250005];
 
 bool valid(int i, int j){
     return i>=0&&i<n&&j>=0&&j<m;
@@ -17,8 +20,7 @@ int bfs(int a, int b){
     q.push({a,b});
     int na,nb;
     int result=0;
-    bool visit[505][505]={0,};
-    visit[a][b]=1;
+    visit[a][b]=group;
 
     while(!q.empty()){
         tie(a,b)=q.front();
@@ -32,7 +34,7 @@ int bfs(int a, int b){
             if(!valid(na,nb)||visit[na][nb]) continue;
 
             if(arr[na][nb]){
-                visit[na][nb]=1;
+                visit[na][nb]=group;
                 q.push({na,nb});
             }
             
@@ -54,13 +56,43 @@ int main() {
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(arr[i][j]==0){
-                arr[i][j]=1;
-                ans=max(ans,bfs(i,j));
-                arr[i][j]=0;
+            if(arr[i][j]==1&&!visit[i][j]){
+                group_size[group]=bfs(i,j);
+                group++;
             }
         }
     }
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cout<<visit[i][j]<<' ';
+    //     }
+    //     cout<<'\n';
+    // }
+
+    for(int i=0;i<n;i++){
+        bool check[250000]={0,};
+        int sum=1;
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==0){
+                for(int k=0;k<4;k++){
+                    int na=dr[k]+i;
+                    int nb=dc[k]+j;
+
+                    if(!valid(na,nb)||!arr[na][nb]||check[visit[na][na]]) continue;
+
+                    if(!check[visit[na][nb]]){
+                        sum+=group_size[visit[na][nb]];
+                        check[visit[na][nb]]=1;
+                    }
+                }
+                //cout<<"a: "<<i<<' '<<j<<' '<<sum<<'\n';
+                ans=max(ans,sum);
+            }
+        }
+    }
+
+
 
     cout<<ans;
 

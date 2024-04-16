13d091091c6289eb5302d260521c7c0e344ab745
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-04-16 21:36:02 +0900

    Title: 1차원 윷놀이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/1차원 윷놀이/yutnori-1d.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,m,k,x,ans=0,temp=0;
+vector<int> v;
+int arr[20];
+
+void proc(int l){
+    if(l==n){
+        int temp=0;
+        for(int i=1;i<=k;i++){
+            if(arr[i]>=m) temp++;
+        }
+        ans=max(ans,temp);
+        return;
+    }
+
+    for(int i=1;i<=k;i++){
+        arr[i]+=v[l];
+        proc(l+1);
+        arr[i]-=v[i];
+    }
+}
+
+int main() {
+    cin>>n>>m>>k;
+
+    for(int i=0;i<m;i++){
+        cin>>x;
+        v.push_back(x);
+    }
+
+    proc(0);
+
+    cout<<ans;
+
+    return 0;
+}

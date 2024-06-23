f69c81705173046091668e348aef2ca46c2184cc
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-06-23 23:36:04 +0900

    Title: 변수 값 복사하기 2, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240623/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=7;
+
+        a=b=c;
+
+        System.out.println(a+" "+b+" "+c);
+    }
+}

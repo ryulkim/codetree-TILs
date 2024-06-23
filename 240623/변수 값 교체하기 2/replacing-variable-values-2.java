044e4f822c1c50718efd0dafd831ed1d40b335b2
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-06-23 23:20:32 +0900

    Title: 변수 값 교체하기 2, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240623/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        a=b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

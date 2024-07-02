4eaa299b0db654ae904d380bd951c4ed631261e6
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-07-02 14:34:41 +0900

    Title: 문자 교체하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240702/문자 교체하기/changing-char.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String s1=sc.next();
+        String s2=sc.next();
+
+        System.out.print(s1.substring(0,2)+s2.substring(2));
+    }
+}

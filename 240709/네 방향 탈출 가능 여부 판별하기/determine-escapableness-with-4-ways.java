8a3f58fd170aaaf440efbb8b218adb4170e76f9e
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-07-09 10:18:53 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 347ms, Memory: 21MB, Status: Passed - CodeTree

--- 240709/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 240709/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -21,7 +21,7 @@ public class Main {
     }
 
     public static boolean valid(int i, int j){
-        return i>=0&&i<n&&j>=0&&j<n;
+        return i>=0&&i<n&&j>=0&&j<m;
     }
 
     public static boolean bfs(){
